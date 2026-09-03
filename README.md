d3a2a847926e1579169fd1e491fc3b4cb61e9f3b
laptop <laptop@laptop.com>
2026-09-03 09:48:49 +0700

    release on emscripten

--- README.md
+++ README.md
@@ -2,3 +2,5 @@ this project successfully compiled RAYLIB+NUKLEAR to web!
 yeayy!!
 
 to generate v "bindings" we use rl.py
+
+run with emrun build-emscripten
--- build-emscripten.sh
+++ build-emscripten.sh
@@ -2,5 +2,5 @@
 set -euo pipefail
 
 v -o main.wasm.c -d emscripten main.v
-emcmake cmake . -B build-emscripten
+emcmake cmake . -B build-emscripten -DCMAKE_BUILD_TYPE=Release
 cmake --build build-emscripten
